db22eeffd6440b4e5dec469c75d6b74b50a22ea8
adiyatan <adiyazam03@gmail.com>
2024-01-05 22:24:03 +0700

    add animation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,18 +19,43 @@
             white-space: pre-wrap;
             word-wrap: break-word;
             font-size: 18px;
+            overflow: hidden;
+            border-right: 2px solid #0F0;
+            animation: typing 4s steps(50) infinite;
+        }
+
+        @keyframes typing {
+            from {
+                width: 0;
+            }
+
+            to {
+                width: 100%;
+            }
+        }
+
+        /* Responsive Styles */
+        @media only screen and (max-width: 600px) {
+            body {
+                margin-top: 10%;
+            }
+
+            pre {
+                font-size: 14px;
+            }
         }
     </style>
 </head>
 
 <body>
     <pre>
-  A   DDDD  III  Y   Y   A   TTTTT   A    N   N
- A A  D   D  I    Y Y   A A    T    A A   NN  N
-AAAAA D   D  I     Y   AAAAA   T   AAAAA  N N N
-A   A DDDD  III    Y   A   A   T   A   A  N  NN
+   A    DDDD  III  Y   Y    A   TTTTT    A     N   N
+  A A   D   D  I    Y Y    A A    T     A A    NN  N
+ AAAAA  D   D  I     Y    AAAAA   T    AAAAA   N N N
+A     A DDDD  III    Y   A     A  T   A     A  N  NN
     </pre>
-    <p>Api</p>
+    <p>==Api==</p>
+    <p>at your service</p>
 </body>
 
 </html>
